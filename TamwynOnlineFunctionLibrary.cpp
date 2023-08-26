86a5bfeed6504f5ed4cf7db158b24312ea9eed7f
Tamwyn001 <50194395+Tamwyn001@users.noreply.github.com>
2023-08-26 21:54:53 +0200

    Added UTamwynOnlineFunctionLibrary .h. cpp
    Added == and to string for FUniqueNetIdRepl

---
+++ TamwynOnlineFunctionLibrary.cpp
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TamwynOnlineFunctionLibrary.h"
+
+
+bool UTamwynOnlineFunctionLibrary::EqualEqual_UniqueNetIdRepl(const FUniqueNetIdRepl& A, const FUniqueNetIdRepl& B)
+{
+	return A.ToString() == B.ToString();
+}
+
+FString UTamwynOnlineFunctionLibrary::Conv_UniqueNetIdReplToString(const FUniqueNetIdRepl& InUniqueNetId)
+{
+	return InUniqueNetId.ToString();
+}
---
+++ TamwynOnlineFunctionLibrary.h
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "OnlineSubsystem.h"
+#include "OnlineSubsystemUtils.h"
+
+#include "TamwynOnlineFunctionLibrary.generated.h"
+
+
+/**
+ * 
+ */
+UCLASS()
+class KINGDOMS_EDGE_API UTamwynOnlineFunctionLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+		/**
+		String-based comparasion of the UniqueNetId. See @UniqueNetIdRepl
+		*/
+	UFUNCTION(BlueprintPure, meta = (DisplayName = "Equal (FUniqueNetId)", CompactNodeTitle = "=="), Category = "Online")
+		static bool EqualEqual_UniqueNetIdRepl(const FUniqueNetIdRepl& A, const FUniqueNetIdRepl& B);
+
+	UFUNCTION(BlueprintPure, meta = (DisplayName = "To String (FUniqueNetId)", CompactNodeTitle = "->", ScriptMethod = "ToString", Category = "Utilities|String")
+		static FString Conv_UniqueNetIdReplToString(const FUniqueNetIdRepl& InUniqueNetId);
+};
